96a4dc662d54d6b3a804b38d2055cfb583c7d2b3
tamim36 <tamimarefinanik@gmail.com>
2022-01-18 18:22:16 +0600

    initital commit

---
+++ Repositories/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositories
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+
+    }
+}
